f9ce10c6c9ef2d907957a8aa7fcf877e97e96a4e
rigenm <109752419+rigenm@users.noreply.github.com>
2022-09-24 15:43:11 -0500

    Add harness provider configuration options
    More explicitly provide harness provider configuration keys and values for expected First/Next Gen resources under dynamic-pipeline example

--- examples/dynamic-pipeline/provider.tf
+++ examples/dynamic-pipeline/provider.tf
@@ -6,6 +6,18 @@ terraform {
   }
 }
 
+# Keep one of the below provider configurations based on your version of Harness
+# Reference: https://registry.terraform.io/providers/harness/harness/latest/docs
+# Harness First Gen resources
 provider "harness" {
-  # Configuration options
+  endpoint   = "https://app.harness.io/gateway"
+  account_id = "...."
+  api_key    = "......"
+}
+
+# Harness Next Gen resources
+provider "harness" {
+  endpoint         = "https://app.harness.io/gateway"
+  account_id       = "...."
+  platform_api_key = "......"
 }
